c9754f23e47cf54e998bdeac05b14658cd64fd3e
BenRStutzman <tysorx89@gmail.com>
2021-01-02 11:19:35 -0500

    Add multi-language support

--- README.md
+++ README.md
@@ -13,19 +13,32 @@ Click the "Yalla" button to begin. The game will prompt you with a verb tense (e
 
 ## Adding verbs
 To add verbs, edit `src/verbs.txt` and then re-build the program with `npm run build`. Just follow the spacing pattern of the existing verbs, or use these instructions if you want them spelled out:
-1. Separate each verb section (e.g. all the conjugations of 'querer')
+1. Separate each language section (e.g. all the Portuguese words) with three blank lines.
+2. The first four lines of each language section should contain:
+  a. The name of the language
+  b. The options for messages to display when the player answers correctly.
+  c. The options for messages to display when the player answers incorrectly.
+  d. The word for "try" in that language, to be used when the player answers incorrectly three times in a row and gets prompted with the correct answer
+For example, the first four lines of the Portuguese section could be:
+
+Portuguese
+correct: Correto!/Ótimo!/Fixe!
+incorrect: Incorreto./Opa./Não.
+try: Tente
+3. Include three blank lines after the header information for each language section
+4. Within each language section, separate each verb section (e.g. all the conjugations of 'querer')
    with two blank lines.
-2. Within each verb section, separate each tense section (e.g. all the
+5. Within each verb section, separate each tense section (e.g. all the
    conjugations in the present tense) with one blank line.
-3. Within each tense section, put the tense name first (e.g. 'presente') on its
+6. Within each tense section, put the tense name first (e.g. 'presente') on its
    own line, and the pronoun/conjugation pairs on the following lines.
-4. Within each pronoun/conjugation pair, separate the pronoun and the
+7. Within each pronoun/conjugation pair, separate the pronoun and the
    conjugation with a space (e.g. 'eu quero').
-5. For conjugations that work for multiple pronouns, separate each pronoun
+8. For conjugations that work for multiple pronouns, separate each pronoun
    with a forward slash (e.g. 'ele/ela/você quer').
-6. For conjugations with multiple correct answers, separate each conjugation
+9. For conjugations with multiple correct answers, separate each conjugation
    with a forward slash (e.g. 'tu diz/dize').
-7. For conjugations that apply to all pronouns, include at least one space
+10. For conjugations that apply to all pronouns, include at least one space
    before the conjugation (e.g. '  querido' for particípio).
 
 ## Sound sources
--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
 import React from 'react';
-import rawFile from './verbs.txt';
+import verbsFile from './verbs.txt';
 import correctSoundFile from './sounds/correct.wav';
 import incorrectSoundFile from './sounds/incorrect.wav';
 
 const correctSound = new Audio(correctSoundFile);
 const incorrectSound = new Audio(incorrectSoundFile);
-
 const incorrectAnswerRetryInterval = 5;
-const correctMessageDisplayTimeSeconds = 10;
-const correctMessagesCorrect = ["Correto!", "Ótimo!", "Fixe!"];
-const correctMessagesIncorrect = ["Incorrecto.", "Não.", "Opa."];
-const problems = [];
-
-fetch(rawFile)
-  .then(result => result.text())
-  .then(text => {
-    let verb;
-    let tenseSets;
-    let tense;
-    let pronouns;
-    let pronounConjugationPairs;
-    let conjugations;
-    const verbSets = text.trim().split(/\n\s*\n\s*\n/);
-    verbSets.forEach(verbSet => {
-      [verb, ...tenseSets] = verbSet.split(/\n\s*\n/);
-      tenseSets.forEach(tenseSet => {
-        [tense, ...pronounConjugationPairs] = tenseSet.split(/\n/);
-        pronounConjugationPairs.forEach(pronounConjugationPair => {
-          [pronouns, conjugations] = pronounConjugationPair.split(/\s+/);
-          pronouns.split('/').forEach(pronoun => {
-            problems.push({
-              question: {
-                tense: tense,
-                pronoun: pronoun,
-                verb: verb
-              },
-              answers: conjugations.split('/')
-            });
-          });
-        });
-      });
-    });
-  });
+const correctMessageDisplayTimeSeconds = 2;
 
 const InputBox = (props) => (
   <input id="input" value={props.value} autoComplete="off" autoFocus
@@ -49,37 +14,103 @@ const InputBox = (props) => (
     onKeyPress={props.handleKeyPress}/>
 );
 
+const defaultState = {
+  languages: null,
+  phase: 'loading',
+  correctMessages: [],
+  incorrectMessages: [],
+  tryMessage: null,
+  problemQueue: [],
+  problems: [],
+  currentProblem: null,
+  mistakesOnCurrentProblem: 0,
+  input: '',
+  correctMessage: {
+    isCorrect: false,
+    message: '',
+  },
+  correctMessageTimeoutId: null,
+  numberOfProblems: 0,
+  numberCorrect: 0
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      problemQueue: [],
-      currentProblem: null,
-      mistakesOnCurrentProblem: 0,
-      input: '',
-      correctMessage: {
-        isCorrect: false,
-        message: '',
-      },
-      correctMessageTimeoutId: null,
-      numberOfProblems: 0,
-      numberCorrect: 0
-    };
+    this.state = defaultState;
 
     this.checkAnswer = this.checkAnswer.bind(this);
     this.updateInput = this.updateInput.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.clearCorrectMessage = this.clearCorrectMessage.bind(this);
     this.begin = this.begin.bind(this);
+    this.chooseLanguage = this.chooseLanguage.bind(this);
+    this.restart = this.restart.bind(this);
   }
 
-  getRandomProblem() {
+  async componentDidMount() {
+    const languages = {};
+    let info;
+    let verbSets;
+    let language;
+    let correctMessages;
+    let incorrectMessages;
+    let tryMessage;
+    let verb;
+    let tenseSets;
+    let tense;
+    let pronouns;
+    let pronounConjugationPairs;
+    let conjugations;
+    fetch(verbsFile)
+      .then(result => result.text())
+      .then(text => {
+
+        const languageSets = text.trim().split(/\n\s*\n\s*\n\s*\n/);
+        languageSets.forEach(languageSet => {
+          [info, ...verbSets] = languageSet.split(/\n\s*\n\s*\n/);
+          [language, correctMessages, incorrectMessages, tryMessage] = info.split('\n');
+          languages[language] = {
+            correctMessages: correctMessages.split(/:\s*/)[1].split('/'),
+            incorrectMessages: incorrectMessages.split(/:\s*/)[1].split('/'),
+            tryMessage: tryMessage.split(/:\s*/)[1],
+            problems: []
+          };
+          verbSets.forEach(verbSet => {
+            [verb, ...tenseSets] = verbSet.split(/\n\s*\n/);
+            tenseSets.forEach(tenseSet => {
+              [tense, ...pronounConjugationPairs] = tenseSet.split(/\n/);
+              pronounConjugationPairs.forEach(pronounConjugationPair => {
+                [pronouns, conjugations] = pronounConjugationPair.split(/\s+/);
+                pronouns.split('/').forEach(pronoun => {
+                  languages[language].problems.push({
+                    question: {
+                      tense: tense,
+                      pronoun: pronoun,
+                      verb: verb
+                    },
+                    answers: conjugations.split('/')
+                  });
+                });
+              });
+            });
+          });
+        });
+        this.setState((state) => Object.assign({}, state, {
+          phase: 'chooseLanguage',
+          languages: languages
+        }));
+      });
+  }
+
+  getRandomProblem(problems = null) {
+    problems = problems ?? this.state.problems;
     return Object.assign({}, problems[Math.floor(Math.random() * problems.length)]);
   }
 
   getRandomCorrectMessage(isCorrect) {
-    const messages = isCorrect ? correctMessagesCorrect : correctMessagesIncorrect;
+    const messages = isCorrect ? this.state.correctMessages : this.state.incorrectMessages;
     return {
       isCorrect: isCorrect,
       message: messages[Math.floor(Math.random() * messages.length)]
@@ -87,13 +118,28 @@ class App extends React.Component {
   }
 
   begin() {
-    let problemQueue = []
-    for (let i = 0; i < incorrectAnswerRetryInterval; i++) {
-      problemQueue.push(this.getRandomProblem());
-    }
     this.setState((state) => Object.assign({}, state, {
+      phase: 'chooseLanguage'
+    }));
+  }
+
+  restart() {
+    this.setState((state) => Object.assign({}, defaultState, {
+      phase: 'chooseLanguage',
+      languages: this.state.languages,
+    }));
+  }
+
+  chooseLanguage(language) {
+    let problemQueue = [];
+    const languageInfo = this.state.languages[language];
+    for (let i = 0; i < incorrectAnswerRetryInterval; i++) {
+      problemQueue.push(this.getRandomProblem(languageInfo.problems));
+    }
+    this.setState((state) => Object.assign({}, state, languageInfo, {
+      phase: 'play',
       problemQueue: problemQueue,
-      currentProblem: this.getRandomProblem()
+      currentProblem: this.getRandomProblem(languageInfo.problems)
     }));
   }
 
@@ -131,7 +177,7 @@ class App extends React.Component {
       incorrectSound.play();
       const correctMessage = this.state.mistakesOnCurrentProblem >= 2 ? {
         isCorrect: false,
-        message: `(tente ${this.state.currentProblem.answers.map(answer => `"${answer}"`).join(' ou ')})`
+        message: `${this.state.tryMessage} ${this.state.currentProblem.answers.map(answer => `"${answer}"`).join(' ou ')}.`
       } : this.getRandomCorrectMessage(false);
       newestState = {
         mistakesOnCurrentProblem: this.state.mistakesOnCurrentProblem + 1,
@@ -162,36 +208,51 @@ class App extends React.Component {
 
   render() {
     let content;
-    if (this.state.currentProblem) {
-      const correctMessageId = this.state.correctMessage.isCorrect ?
-        "correct" : "incorrect";
-      const accuracy = Math.round(this.state.numberCorrect / this.state.numberOfProblems * 100);
-      const score = this.state.numberOfProblems ?
-        `${this.state.numberCorrect}/${this.state.numberOfProblems} (${accuracy}%)` : '';
-      const question = this.state.currentProblem.question;
-      content = (
-        <div id="container">
-          <h2><span className="light-font">{question.tense} {question.pronoun} </span>
-            <span className="extra-bold-font">{question.verb}</span></h2>
-          <InputBox value={this.props.input}
-            handleChange={(event) => this.updateInput(event)}
-            handleKeyPress={(event) => this.handleKeyPress(event)}/>
-          <h2 className="correct-message" id={correctMessageId}>
-            {this.state.correctMessage.message}</h2>
-          <h2>{score}</h2>
-        </div>
-      );
-    } else {
-      content = (
-        <div>
-          <button id="begin-button" onClick={this.begin} autoFocus>Yalla!</button>
-        </div>
-      );
+    switch (this.state.phase) {
+      case 'loading':
+        content = (
+          <h1>loading verbs...</h1>
+        );
+        break;
+      case 'chooseLanguage':
+        content = (
+          <div id="languages">
+            {Object.keys(this.state.languages).map(language => (
+              <button key={language} className="language-button" onClick={() => this.chooseLanguage(language)}>{language}</button>
+            ))}
+          </div>
+        );
+        break;
+      case 'play':
+        const correctMessageId = this.state.correctMessage.isCorrect ?
+          "correct" : "incorrect";
+        const accuracy = Math.round(this.state.numberCorrect / this.state.numberOfProblems * 100);
+        const score = this.state.numberOfProblems ?
+          `${this.state.numberCorrect}/${this.state.numberOfProblems} (${accuracy}%)` : '';
+        const question = this.state.currentProblem.question;
+        content = (
+          <div>
+            <div id="container">
+              <h2><span className="light-font">{question.tense} {question.pronoun} </span>
+                <span className="extra-bold-font">{question.verb}</span></h2>
+              <InputBox value={this.props.input}
+                handleChange={(event) => this.updateInput(event)}
+                handleKeyPress={(event) => this.handleKeyPress(event)}/>
+              <h2 className="correct-message" id={correctMessageId}>
+                {this.state.correctMessage.message}</h2>
+              <h2>{score}</h2>
+            </div>
+            <button id="restart-button" onClick={this.restart}>Restart</button>
+          </div>
+        );
+        break;
+      default:
+        break;
     }
     return(
       <div>
+        <div>{content}</div>
         <h1 id="conjulameos">Conjulameos</h1>
-        {content}
       </div>
     );
   }
